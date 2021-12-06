c98f665adc0d09f85a9398645262f3fa883eabac
polina-g <ignatieva.polina@gmail.com>
2021-12-06 14:07:18 -0500

    adds about us page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi There! My name is Polina and I am a software developer. I'm learning Gatsby to build my portfolio website and currently following the Gatsby tutorial! Wish me luck!</p>
+        </Layout>
+    );
+};
+
+export default AboutPage;
