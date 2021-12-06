6b41c04749e39ccf45b02c168e882df9dc66c138
polina-g <ignatieva.polina@gmail.com>
2021-12-06 17:35:14 -0500

    adds blog posts query and titles to blog page. Finished part 4 of the tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,33 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby';
 
-const BlogPage = () => {
+
+const BlogPage = ({ data }) => {
+    
     return (
         <Layout pageTitle="Blog Posts">
-            <p>My cool posts will go in here!</p>
+            <ul>
+                {
+                    data.allFile.nodes.map(
+                        node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                } 
+            </ul>
         </Layout>
     );
 }
 
-export default BlogPage;
+export const query = graphql`
+    query {
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }`;
+
+  export default BlogPage;
