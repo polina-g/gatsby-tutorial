aab6045d79d5c7f279e4e654b86e9b8d23600017
polina-g <ignatieva.polina@gmail.com>
2021-12-06 14:43:25 -0500

    adds image to index page, finished tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Purple flower"
+        src="../images/flower.jpeg"
+      />
     </Layout>
 
   )
